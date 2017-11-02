3fe9491b45ac28921b6e6d93b07b6b00d9af253b
Dien Phan Thanh <ptdien@tma.com.vn>
2017-11-02 15:06:35 +0700

    -- optimize for user

--- auto-add-friends.js
+++ auto-add-friends.js
@@ -1,4 +1,3 @@
-var logger = new Logger();
 start_auto_add_friends();
 
 function start_auto_add_friends() {
@@ -13,90 +12,70 @@ function scroll_to_bottom() {
 async function auto_add_friends() {
     let maxFriends = parseInt(prompt("Enter how many friends you wanna add: ", "150"));
     if (typeof maxFriends === 'undefined' || !maxFriends) {
-        logger.error("Invalid maxFriends value.");
         if (confirm("Invalid input value. Do you want to enter it again?") == true) {
             start_auto_add_friends();
-        } else {
-            logger.info("Application has been stopped.")
         }
         return;
     }
-    logger.info("Scrolling...");
     for (let i = 0;; i++) {
         scroll_to_bottom();
-        logger.info("Scrolled " + (i + 1) + " times");
         if (i % 5 == 0 || is_end_of_scroll()) {
             clean_trash_cards();
             let addBtn = scan_add_buttons();
             if (addBtn.length < 1) {
-                logger.info("No \"Add Friend\" buttons found.");
                 continue;
             }
-            logger.info("Found " + addBtn.length + " \"Add friend\" buttons");
             if (addBtn.length >= maxFriends || is_end_of_scroll()) {
-                click_add_buttons(addBtn,maxFriends);
+                click_add_buttons(addBtn, maxFriends);
                 break;
             }
         }
         await wait(2000);
     }
-    logger.info("Application has been stopped.");
+    alert("Done.");
     return;
 }
 
 function scan_add_buttons() {
-    logger.info("Scanning for \"Add Friend\" buttons...")
     let addBtn = document.body.querySelectorAll('#contentArea .addButton:not(.hidden_elem)');
     return addBtn;
 }
 
-async function click_add_buttons(addBtn,maxFriends) {
+async function click_add_buttons(addBtn, maxFriends) {
     if (addBtn.length < 1) {
         return;
     }
-    logger.info("Adding...");
     for (let i = 0; i < maxFriends; i++) {
         addBtn[i].click();
         if (i % 10 == 0 || i % (maxFriends - 1) == 0) {
             dismiss_dialogs()
         }
-        logger.info("Added " + (i+1));
         await wait(2000);
     }
 }
 
 function dismiss_dialogs() {
-    logger.info("Scanning for dialogs...");
     let dialog = document.body.querySelectorAll('.uiLayer div[role="dialog"]:not(.uiContextualLayerBelowLeft)');
-
     if (dialog.length < 1) {
-        logger.info("No dialogs found.");
         return;
     }
-    logger.info("Found " + dialog.length + " dialog(s)");
-    logger.info("Dismissing dialog(s)...");
     for (let i = 0; i < dialog.length; i++) {
         if (dialog[i].innerHTML.indexOf("Does This Person Know You") > 0) {
             if (dialog[i].querySelector('.layerConfirm')) {
                 dialog[i].querySelector('.layerConfirm').click();
-            } else {
-                logger.error("Unable to dismiss dialog: " + dialog[i]);
             }
         } else {
             if (dialog[i].querySelector('.layerCancel')) {
                 dialog[i].querySelector('.layerCancel').click();
-            } else {
-                logger.error("Unable to dismiss dialog: " + dialog[i]);
             }
         }
     }
-    logger.info("Done dismissing dialog(s)...");
+
 }
 
 function is_end_of_scroll() {
     let endScroll = document.body.querySelector('._24j');
     if (endScroll !== null) {
-        logger.info("Found \"End of Results\".");
         return true;
     }
     return false;
@@ -111,32 +90,22 @@ async function wait(miliseconds) {
     })
 }
 
-function getTime() {
-    return (new Date()).toUTCString();
-}
-
 function clean_ui() {
     let nope = document.body.querySelectorAll("#pagelet_bluebar,#pagelet_sidebar,#pagelet_dock, #toolbarContainer,#leftCol,#rightCol,#bottomContent");
-    for (let i = 0; i < nope.length; i++) {
-        nope[i].parentNode.removeChild(nope[i]);
+    if (nope != null && nope.length > 0) {
+        for (let i = 0; i < nope.length; i++) {
+            nope[i].parentNode.removeChild(nope[i]);
+        }
     }
 }
 
 function clean_trash_cards() {
     let card = document.body.querySelectorAll("._4p2o");
-    for (let i = 0; i < card.length; i++) {
-        if (!(card[i].innerHTML.match(/Add Friend/))) {
-            card[i].parentNode.removeChild(card[i]);
+    if (nope != null && nope.length > 0) {
+        for (let i = 0; i < card.length; i++) {
+            if (!(card[i].innerHTML.match(/Add Friend/))) {
+                card[i].parentNode.removeChild(card[i]);
+            }
         }
-    }
-    logger.info("Cleaned trash cards.");
-}
-
-function Logger() {
-    this.info = function (message) {
-        console.log("[INFO][" + getTime() + "]: " + message);
-    }
-    this.error = function (message) {
-        console.error("[ERROR][" + getTime() + "]: " + message)
     }
 }
