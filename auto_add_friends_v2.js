b9b2792637e7c6240b2c49ac969636c6cadaa6e2
Phan Thanh Dien <ptdien.lxag@gmail.com>
2017-12-24 10:41:48 +0700

    cancel pending friend request added!

--- auto_add_friends_v2.js
+++ auto_add_friends_v2.js
@@ -4,29 +4,40 @@ async function start (max) {
   let sent = getSent()
   logger.info(`Checking for status...`)
   if (sent.length === max) {
+    window.alert(`Added ${sent.length} out of ${max} friends. Application stopped.`)
     logger.info(`Added enough friends. Application stopped.`)
     return
   }
+  logger.info(`Added ${sent.length} out of ${max} friends.`)
+  /* Not to run cleanTrashCards on first execution */
+  if (sent.length > 0) {
+    /* Clean all cards except for ones that has [Friend Request Sent] buttons */
+    cleanTrashCards()
+  }
   await addFriend(max - sent.length)
   if (isEndOfScroll()) {
+    window.alert(`Hit the bottom. Application stopped.`)
     logger.info('Hit the bottom. Application stopped')
     return
   }
   start(max)
 }
 
+/* Click $max number of  visible [Add Friend] buttons on the screen */
 async function addFriend (max) {
   logger.info(`Need to add (another) ${max} friends.`)
+  logger.info(`Scanning...`)
   for (let i = 0; ; i++) {
     let notSent = getNotSent()
-    logger.info(`Found ${notSent.length} add buttons`)
     if (notSent.length >= max) {
+      logger.info(`Found ${notSent.length} [Add Friend] buttons.`)
+      logger.info(`Adding...`)
       for (let j = 0; j < max; j++) {
         notSent[j].click()
-        logger.info(`Clicked ${j + 1} add button(s)`)
         await wait(2000)
         dismissDialogs()
       }
+      logger.info(`Done adding.`)
       break
     }
     scrollToBottom()
@@ -34,14 +45,17 @@ async function addFriend (max) {
   }
 }
 
+/* Get all visible [Friend Request Sent] buttons on the screen */
 function getSent () {
   return document.querySelectorAll('.FriendRequestOutgoing:not(.hidden_elem)')
 }
 
+/* Get all visible [Add Friend] buttons on the screen */
 function getNotSent () {
   return document.body.querySelectorAll('.FriendRequestAdd:not(.hidden_elem)')
 }
 
+/* Get max amount of friends will be Added. Default: 100 */
 function getMax () {
   let max = parseInt(window.prompt('Enter how many friends you wanna add: ', '100'))
   if (isNaN(max)) {
@@ -69,6 +83,16 @@ function isEndOfScroll () {
   return false
 }
 
+function cleanTrashCards () {
+  let card = document.body.querySelectorAll('._4p2o')
+  for (let i = 0; i < card.length; i++) {
+    if (!card[i].querySelector('.FriendRequestOutgoing:not(.hidden_elem)')) {
+      card[i].parentNode.removeChild(card[i])
+    }
+  }
+  logger.info('Cleaned trash cards.')
+}
+
 function dismissDialogs () {
   let dialog = document.body.querySelectorAll('.uiLayer div[role="dialog"]:not(.uiContextualLayerBelowLeft)')
   for (let i = 0; i < dialog.length; i++) {
@@ -82,7 +106,6 @@ function dismissDialogs () {
       }
     }
   }
-  logger.info('Dismissed dialog(s).')
 }
 
 function Logger () {
---
+++ cancel_pending_friend_requests.js
@@ -0,0 +1,123 @@
+var logger = new Logger()
+
+start(100)
+
+/* Start the application, $size determines how many outgoing requests will be cancel */
+async function start (size) {
+  let token = getToken()
+  let user = getUserId()
+  /* Pause application and wait for getPending() is done */
+  let friends = await getPending(getUserId(), size).then((data) => {
+    return data
+  }).catch((error) => {
+    logger.error(error)
+  })
+  if (friends.length === 0) {
+    window.alert('No pending outgoing request found. Application stopped.')
+    return
+  }
+  for (let i = 0; i < friends.length; i++) {
+    /* Pause application and wait until cancelRequest() is done */
+    await cancelRequest(user, friends[i].match(/\d+/)[0], token).then((response) => {
+      logger.info(response)
+    }).catch((error) => {
+      logger.error(error)
+    })
+    /* Pause application for x millisecond(s) */
+    await wait(2000)
+  }
+  window.alert('Done')
+}
+
+/* Get pending outgoing request(s) */
+function getPending (user, size) {
+  return new Promise((resolve, reject) => {
+    let url = `/friends/requests/outgoing/more/?page=1&page_size=${size}&pager_id=outgoing_reqs_pager_5a3e76a99ad4e6951982620&dpr=1&__user=${user}&__a=1`
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.responseText.match(/profileid=\\"\d+\\"/g)) {
+          resolve(xhr.responseText.match(/profileid=\\"\d+\\"/g))
+        } else {
+          resolve([])
+        }
+      }
+    }
+
+    xhr.send()
+  })
+}
+
+/* Cancel one outgoing pending request */
+function cancelRequest (user, friend, token) {
+  return new Promise((resolve, reject) => {
+    let params = `friend=${friend}&cancel_ref=outgoing_requests&__user=${user}&__a=1&fb_dtsg=${token}&confirmed=1`
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', '/ajax/friends/requests/cancel.php?dpr=1', true)
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=utf-8')
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        resolve(`Canceled request to ${friend}`)
+      }
+    }
+    xhr.send(params)
+  })
+}
+
+function getUserId () {
+  let myId = null
+  if (document.cookie.match(/c_user=(\d+)/)) {
+    if (document.cookie.match(/c_user=(\d+)/)[1]) {
+      myId = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])
+      return myId
+    }
+  } else {
+    myId = window.prompt('ID not found in cookies, please enter it manually: ', '')
+    return myId
+  }
+}
+
+function getToken () {
+  let fb = null
+  if (window.location.pathname.match('/pokes')) {
+    try {
+      fb = document.documentElement.innerHTML.match(/,\{"token":"\(.\*\?\)"/g)[0].replace(',\{"token":"', '').replace('"', '')
+      return fb
+    } catch (e) {
+      logger.error(e)
+    };
+  } else {
+    try {
+      if (document.getElementsByName('fb_dtsg')) {
+        if (document.getElementsByName('fb_dtsg')[0]) {
+          fb = document.getElementsByName('fb_dtsg')[0].value
+          return fb
+        }
+      }
+    } catch (e) {
+      logger.error(e)
+    };
+  };
+}
+
+function Logger () {
+  this.info = function (message) {
+    console.log('[INFO][' + getTime() + ']: ' + message)
+  }
+  this.error = function (message) {
+    console.error('[ERROR][' + getTime() + ']: ' + message)
+  }
+}
+
+function getTime () {
+  return (new Date()).toUTCString()
+}
+
+function wait (milliseconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve()
+    }, milliseconds)
+  })
+}
